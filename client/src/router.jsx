46259621bb3361fc16df6e39d28072aa8c4a44f8
Marcos Vinicius Pereira <marcosvinipereira4@gmail.com>
2024-05-18 20:42:37 -0300

    Migrating front to vite environment

---
+++ client/src/router.jsx
@@ -0,0 +1,25 @@
+import { React } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "../src/styles/global.css"
+
+import Login from "./views/pages/Login/login.jsx";
+import Register from "./views/pages/Register/register.jsx";
+import Home from "./views/pages/Home/home.jsx";
+import Profile from "./views/pages/Profile/Profile.jsx";
+
+function Router() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route element={ <Login /> } path="/" exact/>
+                <Route element={ <Register /> } path="/register" exact/>
+                <Route element = { <Home /> } path="/home" exact/>
+                <Route element = { <Profile /> } path="/profile" exact/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
